dc12a6d230b4a4219f94348c935830f74cced978
Рафаэль Хасанов <rap1raf@gmail.com>
2019-08-14 00:00:03 +0300

    1

---
+++ AutoLotDAL_Core2/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AutoLotDAL_Core2.TestDriver")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoLotDAL_Core2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoLotDAL_Core2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50f3b4ed-76d2-442e-9c6c-9b37c5f427fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- AutoLotDAL_Core2/EF/AutoLotContext.cs
+++ AutoLotDAL_Core2/EF/AutoLotContext.cs
@@ -9,9 +9,9 @@ namespace AutoLotDAL_Core2.EF
     {
         public AutoLotContext(DbContextOptions options) : base(options)
         {
-            
         }
 
+        internal AutoLotContext() { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -23,5 +23,26 @@ namespace AutoLotDAL_Core2.EF
                     .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
             }
         }
+
+        public DbSet<AutoLotDAL_Core2.Models.CreditRisk> CreditRisks { get; set; }
+        public DbSet<AutoLotDAL_Core2.Models.Customer> Customers { get; set; }
+        public DbSet<AutoLotDAL_Core2.Models.Inventory> Cars { get; set; }
+        public DbSet<AutoLotDAL_Core2.Models.Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Создать индекс, включающий в себя несколько столбцов
+            modelBuilder.Entity<CreditRisk>(entity =>
+            {
+                entity.HasIndex(e => new {e.FirstName, e.LastName}).IsUnique();
+            });
+            //Установить параметр каскадирования на отношение
+            modelBuilder.Entity<Order>()
+                .HasOne(e => e.Car)
+                .WithMany(e => e.Orders)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+
+        public string GetTableName(Type type) => Model.FindEntityType(type).SqlServer().TableName;
     }
 }
